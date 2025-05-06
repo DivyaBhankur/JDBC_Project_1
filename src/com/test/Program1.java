9d943863a55623de9733a0a70cbd076f40cdc326
DivyaBhankur <151900655+DivyaBhankur@users.noreply.github.com>
2025-05-06 09:31:56 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -1,8 +1,13 @@
 package com.test;
-
+import java.util.Scanner;
 public class Program1 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n=sc.nextInt();
+		int square_num=n*n;
+		System.out.println("The square of the number "+n "is "+square_num);
 		System.out.println("Hello World!");
 	}
 
